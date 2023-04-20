bc14f2dcb7e460d011213960540e60f392b1a18f
zawho <zthooper2@gmail.com>
2023-04-20 15:21:56 +0800

    Finish basic javascript module logic

--- main.js
+++ main.js
@@ -10,6 +10,16 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/contact.js":
+/*!************************!*\
+  !*** ./src/contact.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction loadContact() {\n    const contentDiv = document.getElementById('content');\n\n    const contactDiv = document.createElement('div');\n    contactDiv.className = 'contact-div';\n    contactDiv.innerText = `if you have any issue please do not hesitate to call...\n    you may call Mr. Chef directly...\n    PHONE: 666-666-6666\n    or you may email Mr. Chef....\n    EMAIL: iamthechef@chefmail.com\n    please visit our restaurant....\n    located in **********\n`\n    contentDiv.appendChild(contactDiv);\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (loadContact);\n\n//# sourceURL=webpack://restaurant-page/./src/contact.js?");
+
+/***/ }),
+
 /***/ "./src/home.js":
 /*!*********************!*\
   !*** ./src/home.js ***!
@@ -26,7 +36,17 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home */ \"./src/home.js\");\n\n\n\nfunction createHeader() {\n    const docBody = document.querySelector('body');\n\n    const header = document.createElement('header');\n    header.className = 'header';\n\n    const homeTab = document.createElement('button');\n    homeTab.className = 'home-tab';\n    homeTab.innerText = 'home';\n\n    const menuTab = document.createElement('button');\n    menuTab.className = 'menu-tab';\n    menuTab.innerText = 'menu';\n\n    const contactTab = document.createElement('button');\n    contactTab.className = 'contact-tab';\n    contactTab.innerText = 'contact';\n\n    header.appendChild(homeTab);\n    header.appendChild(menuTab);\n    header.appendChild(contactTab);\n    docBody.prepend(header);\n}\n\ncreateHeader();\n(0,_home__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home */ \"./src/home.js\");\n/* harmony import */ var _menu__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./menu */ \"./src/menu.js\");\n/* harmony import */ var _contact__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./contact */ \"./src/contact.js\");\n\n\n\n\nconst homeTab = document.createElement('button');\nconst menuTab = document.createElement('button');\nconst contactTab = document.createElement('button');\nconst contentDiv = document.getElementById('content');\n\nfunction createHeader() {\n    const docBody = document.querySelector('body');\n\n    const header = document.createElement('header');\n    header.className = 'header';\n\n    homeTab.className = 'home-tab';\n    homeTab.innerText = 'home';\n\n    menuTab.className = 'menu-tab';\n    menuTab.innerText = 'menu';\n\n    contactTab.className = 'contact-tab';\n    contactTab.innerText = 'contact';\n\n    header.appendChild(homeTab);\n    header.appendChild(menuTab);\n    header.appendChild(contactTab);\n    docBody.prepend(header);\n}\n\nfunction switchTab() {\n    contentDiv.innerHTML = '';\n    if (this === menuTab) {\n        (0,_menu__WEBPACK_IMPORTED_MODULE_1__[\"default\"])();\n    } else if (this === contactTab) {\n        (0,_contact__WEBPACK_IMPORTED_MODULE_2__[\"default\"])();\n    } else {\n        (0,_home__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n    }\n}\n\nhomeTab.addEventListener('click', switchTab);\nmenuTab.addEventListener('click', switchTab);\ncontactTab.addEventListener('click', switchTab);\n\ncreateHeader();\n(0,_home__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/menu.js":
+/*!*********************!*\
+  !*** ./src/menu.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction loadMenu() {\n    const contentDiv = document.getElementById('content');\n\n    const menuList = document.createElement('ul');\n    menuList.className = 'menu-list'; \n\n    const menuItem1 = document.createElement('li');\n    menuItem1.className = 'menu-items';\n    menuItem1.innerText = `mystery pizzaaaa\n    we can't tell u the ingredients... good luck`;\n\n    const menuItem2 = document.createElement('li');\n    menuItem2.className = 'menu-items';\n    menuItem2.innerText = `saucy serpents\n    succulent serpentine snacks... suggest... satisfied slurping...`;\n\n    const menuItem3 = document.createElement('li');\n    menuItem3.className = 'menu-items';\n    menuItem3.innerText = `mug-o-glug\n    GLUG GLUG GLUG GLUG GLUG`;\n\n    menuList.appendChild(menuItem1);\n    menuList.appendChild(menuItem2);\n    menuList.appendChild(menuItem3);\n    contentDiv.appendChild(menuList);\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (loadMenu);\n\n\n//# sourceURL=webpack://restaurant-page/./src/menu.js?");
 
 /***/ })
 
